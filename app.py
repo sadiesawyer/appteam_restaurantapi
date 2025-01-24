9f390af38d1706b7772a7951f4374ba78ff0d0b0
Sadie Sawyer <sadies@unc.edu>
2025-01-24 15:15:37 -0500

    created GET restaurant endpoint

--- app.py
+++ app.py
@@ -10,3 +10,26 @@ db.init_app(app)
 ma.init_app(app)
 migrate = Migrate(app, db)
 
+#initialize schemas
+restaurant_schema = RestaurantSchema()
+restaurants_schema = RestaurantSchema(many=True)
+review_schema = ReviewSchema()
+reviews_schema = ReviewSchema(many=True)
+
+with app.app_context():
+    db.create_all()
+
+#endpoint to add a restaurant to the list
+@app.route("/restaurants", methods=["POST"])
+def add_restaurant():
+    data = request.json
+    new_restaurant = Restaurant(
+        name=data["name"], 
+        cuisine_type=data["cuisine_type"]
+    )
+    db.session.add(new_restaurant)
+    db.session.commit()
+    return restaurant_schema.jsonify(new_restaurant)
+
+if __name__ == "__main__":
+    app.run(debug=True)
